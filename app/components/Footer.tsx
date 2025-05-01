c88fffea29615e2a2d9e4a4edad70b1243bdbe3f
cam lowndes <cameronlowndes@gmail.com>
2025-05-01 14:09:25 +0100

    done for today

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,13 +7,13 @@ export default function Footer() {
         <footer className="w-full bg-gray-800 text-black shadow-md py-4 px-8 flex justify-center items-center fixed bottom-0 left-0 right-0 z-50">
             <p className="text-sm text-center text-white">
                 &copy; {currentYear} Cameron Lowndes | Follow me on{" "}
-                <a href="https://x.com/cameron_lowndes" className="underline" target="_blank" rel="noopener noreferrer">
+                <a href="https://x.com/works_cameron" className="underline" target="_blank" rel="noopener noreferrer">
                     Twitter
                 </a>{" "} |{" "}
                 <Link href="/terms" className="underline">
                     Terms
                 </Link>{" "} |{" "}
-                <a href="https://bsky.app/profile/cameronlowndes.bsky.social" className="underline" target="_blank" rel="noopener noreferrer">
+                <a href="https://bsky.app/profile/camerondigitalwork.bsky.social" className="underline" target="_blank" rel="noopener noreferrer">
                    bluesky
                 </a>
             </p>
